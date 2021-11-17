c48d98c5ce79117c2f632e73cd3bb833fe180850
Piotr Lach <mcmoher59@gmail.com>
2021-11-17 22:38:53 +0100

    Added JUnit test for Bishop

---
+++ src/test/java/my/chess/pieces/BishopTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2021 Piotr Lach
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package my.chess.pieces;
+
+import java.awt.Color;
+import java.util.List;
+import my.chess.Square;
+import my.chess.Coord;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * @author Piotr Lach
+ */
+public class BishopTest {
+
+    private final Bishop bishop = new Bishop(Color.BLACK, null);
+    private final Coord from = new Coord(3,3);
+    private final Square source = new Square(from);
+
+    @Test
+    public void testIsCorrectMovement() {
+        var correctCases = List.of(
+                new Coord(4, 4),
+                new Coord(2, 2),
+                new Coord(4, 2),
+                new Coord(2, 4)
+        );
+        for (var to : correctCases) {
+            var target = new Square(to);
+
+            var isCorrect = bishop.isCorrectMovement(source, target);
+            assertTrue(isCorrect);
+        }
+    }
+
+    @Test
+    public void testIsIncorrectMovement() {
+        var incorrectCases = List.of(
+                new Coord(4, 5),
+                new Coord(3, 5),
+                new Coord(3, 1),
+                new Coord(4, 3)
+        );
+
+        for (var to : incorrectCases) {
+            var target = new Square(to);
+
+            var isIncorrect = !bishop.isCorrectMovement(source, target);
+            assertTrue(isIncorrect);
+        }
+    }
+}
